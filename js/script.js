572a56bff676a432de46ca7c16bbb21cc1c4087a
Uyas <li3shuo1@gmail.com>
2018-03-20 15:49:05 -0400

    Fixed For Firefox
    Tested: Chrome, Firefox
    Not working: MS Edge, IE

--- js/script.js
+++ js/script.js
@@ -1,61 +1,19 @@
-var LEN = 20; 
-var MIN_RATIO = 0.45; 
-var MAX_RATIO = 0.55; 
-var PERFECT_RATIO = 0.5;
-
-function Update() {
-	LEN = $("#query").val(); 
-	MIN_RATIO =  (parseInt($("#min_ratio").val())) / 100; 
-	MAX_RATIO =  (parseInt($("#max_ratio").val())) / 100; 
-	PERFECT_RATIO = (parseInt($("#perfect_ratio").val())) / 100; 
-	$('#in_a').val( $('#in_a').val().toUpperCase() );
-	$('#in_b').val( $('#in_b').val().toUpperCase() );
-}
-
-function UDChar(ch) {
-	var ans = '';  
-	if (ch == 'A') {
-		ans = 'T'; 
-	}
-	else if (ch == 'T') {
-		ans = 'A'; 
-	}
-	else if (ch == 'U') {
-		ans = 'A'; 
-	}
-	else if (ch == 'C') {
-		ans = 'G'; 
-	}
-	else if (ch == 'G') {
-		ans = 'C'; 
-	}
-	return ans; 
-}
-
 function Trim(str) {
 	return str.trim().replace(/[\r\n\s]/g,"");
 }
 
-function UpsideDown(str){
-	str = Trim(str);
-	var ans = ""; 
-	var n = str.length; 
-	for (var i = 0; i < n; ++i) ans = ans.concat(UDChar(str[n-i-1]));
-	return ans;  
-}
-
 function Analyze(a) {
 	var res = a.trim();
-	// begin abstract
-	var abs = /\\begin\{.+\}(.+)\\end\{.+\}/gs;
-	var match = abs.exec(res);
+	// begin re_abstract
+	var re_abs = /\\begin\{.+\}(.+)\\end\{.+\}/gm;
+	var match = re_abs.exec(res);
 	if (match && match[1]) {
 		res = match[1];
 	}
 
-	// ieee \abstract
-	abs = /\\abstract{(.+)}/gs;
-	match = abs.exec(res);
+	// ieee \re_abstract
+	re_abs = /\\re_abstract{(.+)}/gm;
+	match = re_abs.exec(res);
 	if (match && match[1]) {
 		res = match[1];
 	}
@@ -64,11 +22,11 @@ function Analyze(a) {
 	res = res.replace(/\\degree/g, "°");
 	res = res.replace(/\\etal/g, "et al.");
 	// comments
-	res = res.replace(/(?<!\\)%.+/g, "");
+	res = res.replace(/([^\\]|^)%.+/gm, ""); // Fixed for Firefox
 	// emph
-	res = res.replace(/\{\\\w+/gs, "").replace(/\\\/\}/g, "");
+	res = res.replace(/\{\\\w+/gm, "").replace(/\\\/\}/g, "");
 	// textit, $, and ~
-	res = res.replace(/\\\w+{/gs, "").replace(/[\}\$]/g, "").replace(/\~/g, " ");
+	res = res.replace(/\\\w+{/gm, "").replace(/[\}\$]/g, "").replace(/\~/g, " ");
 	// double white spaces
 	res = res.replace(/\n/g, " ");
 	res = res.replace(/\s\s+/g, " ");
@@ -76,6 +34,7 @@ function Analyze(a) {
 	// \% percentage
 	res = res.replace(/\\\%/g, "%");
 	res = res.trim(); 
+
 	return res; 
 }
 
