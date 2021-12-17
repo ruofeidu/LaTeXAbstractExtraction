753bd8a14d3ff09eec43c381fabc0158a38de3ae
Ruofei Du <xiaoxinghai@gmail.com>
2021-12-17 15:15:06 -0800

    Added sim.

--- js/script.js
+++ js/script.js
@@ -1,84 +1,88 @@
 "use strict";
 
 function Trim(str) {
-	return str.trim().replace(/[\r\n\s]/g,"");
+  return str.trim().replace(/[\r\n\s]/g, "");
 }
 
 function Analyze(a) {
-	var res = a.trim();
-	// begin re_abstract
-	var re_abs = /\\begin\{.+\}(.+)\\end\{.+\}/gm;
-	var match = re_abs.exec(res);
-	if (match && match[1]) {
-		res = match[1];
-	}
+  var res = a.trim();
+  // begin re_abstract
+  var re_abs = /\\begin\{.+\}(.+)\\end\{.+\}/gm;
+  var match = re_abs.exec(res);
+  if (match && match[1]) {
+    res = match[1];
+  }
 
-	// ieee \re_abstract
-	re_abs = /\\re_abstract{(.+)}/gm;
-	match = re_abs.exec(res);
-	if (match && match[1]) {
-		res = match[1];
-	}
-	
-	// citations
-	res = res.replace(/\\cite\{[\w\d,:]+\}/g, "");
-	res = res.replace(/\\ref\{[\w\d,:]+\}/g, "X");
-	res = res.replace(/\\begin\{[\w\d,:]+\}\[.+\]/g, "");
-	res = res.replace(/\\end\{[\w\d,:]+\}/g, "");
-	res = res.replace(/\\label\{[\w\d,:]+\}/g, "");
-	res = res.replace(/\\centering/g, "");
-	res = res.replace(/\\caption/g, "");
-	res = res.replace(/\\includegraphics[\[\w\d\,\.\:\=\/\\]+\]\{[\w\d,\.\:\/\\\_]+\}/g, "");
-	
-	// latex symbols
-	res = res.replace(/\\degree/g, "°");
-	res = res.replace(/\\times/g, "×");
-	res = res.replace(/\\etal/g, "et al.");
-	res = res.replace(/``/g, '"');
-	res = res.replace(/""/g, '"');
-	res = res.replace(/ \./g, '.');
-	
-	// comments
-	res = res.replace(/([^\\]|^)%.+/gm, ""); // Fixed for Firefox
-	
-	// emph and italics
-	res = res.replace(/\{\\\w+/gm, "").replace(/\\\/\}/g, "");
-	
-	// textit, $, and ~
-	res = res.replace(/\\\w+{/gm, "").replace(/[\}\$]/g, "").replace(/\~/g, " ");
-	
-	// double white spaces
-	res = res.replace(/\n/g, " ");
-	res = res.replace(/\s\s+/g, " ");
-	
-	// \% percentage
-	res = res.replace(/\\\%/g, "%");
-	res = res.trim(); 
+  // ieee \re_abstract
+  re_abs = /\\re_abstract{(.+)}/gm;
+  match = re_abs.exec(res);
+  if (match && match[1]) {
+    res = match[1];
+  }
 
-	return res; 
+  // citations
+  res = res.replace(/\\cite\{[\w\d,:]+\}/g, "");
+  res = res.replace(/\\ref\{[\w\d,:]+\}/g, "X");
+  res = res.replace(/\\begin\{[\w\d,:]+\}\[.+\]/g, "");
+  res = res.replace(/\\end\{[\w\d,:]+\}/g, "");
+  res = res.replace(/\\label\{[\w\d,:]+\}/g, "");
+  res = res.replace(/\\centering/g, "");
+  res = res.replace(/\\caption/g, "");
+  res = res.replace(
+    /\\includegraphics[\[\w\d\,\.\:\=\/\\]+\]\{[\w\d,\.\:\/\\\_]+\}/g,
+    ""
+  );
+
+  // latex symbols
+  res = res.replace(/\\degree/g, "°");
+  res = res.replace(/\\times/g, "×");
+  res = res.replace(/\\etal/g, "et al.");
+  res = res.replace(/``/g, '"');
+  res = res.replace(/""/g, '"');
+  res = res.replace(/ \./g, ".");
+
+  // comments
+  res = res.replace(/([^\\]|^)%.+/gm, ""); // Fixed for Firefox
+
+  // emph and italics
+  res = res.replace(/\{\\\w+/gm, "").replace(/\\\/\}/g, "");
+
+  // textit, $, and ~
+  res = res
+    .replace(/\\\w+{/gm, "")
+    .replace(/[\}\$]/g, "")
+    .replace(/\~/g, " ");
+  res = res.replace(/\\sim/g, "~");
+
+  // double white spaces
+  res = res.replace(/\n/g, " ");
+  res = res.replace(/\s\s+/g, " ");
+
+  // \% percentage
+  res = res.replace(/\\\%/g, "%");
+  res = res.trim();
+
+  return res;
 }
 
-$(document).ready(function() {
-	$("#in_a").mouseover(function(){
-		$(this).select();
-	});
+$(document).ready(function () {
+  $("#in_a").mouseover(function () {
+    $(this).select();
+  });
 
-	$("#out").mouseover(function(){
-		$(this).select();
-	});
+  $("#out").mouseover(function () {
+    $(this).select();
+  });
 
-	$('#analyze').click(function(){
-		$('#out').val(Analyze($('#in_a').val())); 
-	}); 
-	
-	$('#in_a').keyup(function(){
-		$('#out').val(Analyze($('#in_a').val())); 
-    });
-	
-	$('#analyze').click();
-	
-	$("a").attr("target", "_blank");
+  $("#analyze").click(function () {
+    $("#out").val(Analyze($("#in_a").val()));
+  });
+
+  $("#in_a").keyup(function () {
+    $("#out").val(Analyze($("#in_a").val()));
+  });
+
+  $("#analyze").click();
+
+  $("a").attr("target", "_blank");
 });
-
-
-
